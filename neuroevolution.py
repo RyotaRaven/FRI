47ced6f42baa412e0b1274ebd832f8ca730d2a40
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-02-24 19:52:58 -0600

    NAND WORKS

--- neuroevolution.py
+++ neuroevolution.py
@@ -3,11 +3,6 @@ class Neuron:
     bias=1
     global weights
     weights=[0,0,0,0]
-    #int i[]={1,2,3,4}
-    #i[0]=5
-    #{5,2,3,4}
-    global summation
-    summation=0
     def __init__(self):
         self.data=[]
     def NAND(self, i1,i2,i3):
@@ -21,6 +16,7 @@ class Neuron:
             x=weights[i]
             y=inputs[i]
             summation+=(x*y)
+        return summation
     def OR(self, i1,i2,i3):
         summation=0
         inputs=[bias,i1,i2,i3]
@@ -30,17 +26,35 @@ class Neuron:
         weights[3]=0
         for i in range(0,4):
             summation= summation+(weights[i]*inputs[i])
-    def Output(self):
-        if(summation>1):
+        return summation
+    def Output(self,x):
+        if(x>1):
             return True
         else:
             return False
 
 n=Neuron() #Neuron n= new Neuron();
-n.NAND(0,0,0)
-t1= n.Output()
+i=n.NAND(0,0,0)
+t1= n.Output(i)
 print t1
-n.NAND(0,1,0)
-t2= n.Output()
+i=n.NAND(0,0,1)
+t2= n.Output(i)
 print t2
-    
+i=n.NAND(0,1,0)
+t3= n.Output(i)
+print t3
+i=n.NAND(1,0,0)
+t4= n.Output(i)
+print t4
+i=n.NAND(0,1,1)
+t5= n.Output(i)
+print t5
+i=n.NAND(1,1,0)
+t6= n.Output(i)
+print t6
+i=n.NAND(1,0,1)
+t7= n.Output(i)
+print t7
+i=n.NAND(1,1,1)
+t8= n.Output(i)
+print t8

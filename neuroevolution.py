b63704c26a36ba778082997c728288313134e492
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-02-24 19:57:09 -0600

    ORRRRRR

--- neuroevolution.py
+++ neuroevolution.py
@@ -20,10 +20,10 @@ class Neuron:
     def OR(self, i1,i2,i3):
         summation=0
         inputs=[bias,i1,i2,i3]
-        weights[0]=1
-        weights[1]=0
-        weights[2]=0
-        weights[3]=0
+        weights[0]=0
+        weights[1]=2
+        weights[2]=2
+        weights[3]=2
         for i in range(0,4):
             summation= summation+(weights[i]*inputs[i])
         return summation
@@ -34,6 +34,7 @@ class Neuron:
             return False
 
 n=Neuron() #Neuron n= new Neuron();
+print "NAND:"
 i=n.NAND(0,0,0)
 t1= n.Output(i)
 print t1
@@ -56,5 +57,30 @@ i=n.NAND(1,0,1)
 t7= n.Output(i)
 print t7
 i=n.NAND(1,1,1)
+t8= n.Output(i)
+print t8
+print "OR:"
+i=n.OR(0,0,0)
+t1= n.Output(i)
+print t1
+i=n.OR(0,0,1)
+t2= n.Output(i)
+print t2
+i=n.OR(0,1,0)
+t3= n.Output(i)
+print t3
+i=n.OR(1,0,0)
+t4= n.Output(i)
+print t4
+i=n.OR(0,1,1)
+t5= n.Output(i)
+print t5
+i=n.OR(1,1,0)
+t6= n.Output(i)
+print t6
+i=n.OR(1,0,1)
+t7= n.Output(i)
+print t7
+i=n.OR(1,1,1)
 t8= n.Output(i)
 print t8

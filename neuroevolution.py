2447f8ed6c6843682adeb9bb87b8c9384213b22d
typicalTYLER <TylerBusby333@gmail.com>
2014-02-25 19:15:35 -0600

    added more comments
    pls luv my neuron

--- neuroevolution.py
+++ neuroevolution.py
@@ -3,11 +3,15 @@
 #Randomly change the weights to see what is better
 #decide the fittness based on how many things are correct based on what it SHOULD  be like: if something should be F,F,F and it gives F,F,T the fittness is true
 
+#This design for the neuron object will be easier to genetically alter, because each neuron will have an array of weights
+#In addition, we found out that python doesn't use getter or setter methods in ojects, but this thing called properties
+#so for all the object variables you can just do like myNeuron.weights = [1,2,3] or myNeuron.bias = orNeuron.bias etc.
+
 class Neuron:
     bias = 0
     weights = []
     #I figured you might want to change the amount of inputs in the future so this
-    #constructor initializes a neuron with bias one and variable length of weights
+    #constructor initializes a neuron with a given bias and an array of weights with variable length
     def __init__(self,weights,bias):
         self.bias = bias
         self.weights = weights
@@ -25,6 +29,8 @@ class Neuron:
 
 #TEST CASES
 nandNeuron = Neuron([-1,-1,-1],-3) #initialize the nand neuron with appropriate weights
+#I THINK I HAVE PYTHON 3 ON MY COMPUTER BECAUSE IT WON'T LET ME PRINT WITHOUT PARENTHESES
+#I THINK/HOPE IT STILL WORKS ON YOUR MACHINES
 print ("Our NAND:")
 #NAND returns true for everything except 1,1,1 (ACTUAL NAND)
 print (nandNeuron.eval([0,0,0]))
@@ -38,6 +44,7 @@ print (nandNeuron.eval([1,1,1]))
 
 orNeuron = Neuron([1,1,1],0) #initialize the or neuron with appropriate weights and 
 print ("Our OR:")
+#OR returns true for everything except 0,0,0
 print (orNeuron.eval([0,0,0]))
 print (orNeuron.eval([0,0,1]))
 print (orNeuron.eval([0,1,0]))

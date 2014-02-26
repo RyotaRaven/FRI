8a4a029f8d6de5960c3549e1f9a36e19d3e728df
typicalTYLER <TylerBusby333@gmail.com>
2014-02-25 18:52:35 -0600

    cleaned up code and generalized
    generalized the neuron class to create a neuron with a given bias and
    weights (in array form) which now has a single eval method that takes in
    an array of inputs which will return true or false based on whether the
    neuron fired or not

--- neuroevolution.py
+++ neuroevolution.py
@@ -4,113 +4,45 @@
 #decide the fittness based on how many things are correct based on what it SHOULD  be like: if something should be F,F,F and it gives F,F,T the fittness is true
 
 class Neuron:
-    global bias
-    bias=1
-    global weights
-    weights=[0,0,0,0]
-    def __init__(self):
-        self.data=[]
-    def NAND(self, i1,i2,i3): #Weights that work for NAND whoo
-        summation=0
-        inputs=[bias,i1,i2,i3]
-        weights[0]=6 #-2+-2+-2 overrides the 6
-        weights[1]=-2
-        weights[2]=-2
-        weights[3]=-2
-        for i in range(0,4):
-            x=weights[i]
-            y=inputs[i]
-            summation+=(x*y)
-        return summation
-    def OR(self, i1,i2,i3): #Weights the work for OR whoo
-        summation=0
-        inputs=[bias,i1,i2,i3]
-        weights[0]=0
-        weights[1]=2
-        weights[2]=2
-        weights[3]=2
-        for i in range(0,4):
-            summation= summation+(weights[i]*inputs[i])
-        return summation
-    def Output(self,x):
-        if(x>1):
-            return True
-        else:
-            return False
-
-class EvoNeuron:
-    global bias
-    bias=1 #bias is always 1
-    global weights
-    def __init__(self):
-        self.data=[]
-    def Summation(self, i1,i2,i3,w0,w1,w2,w3): #just does the function you want based on the weights given and calculates the sum
-        summation=0                            #...yeah so its like all universal and stuff
-        inputs=[bias,i1,i2,i3]
-        weights=[w0,w1,w2,w3]
-        for i in range(0,4):
-            x=weights[i]
-            y=inputs[i]
-            summation+=(x*y)
-        return summation
-    def Output(self,x):
-        if(x>1):
-            return True
-        else:
-            return False
+    bias = 0
+    weights = []
+    #I figured you might want to change the amount of inputs in the future so this
+    #constructor initializes a neuron with bias one and variable length of weights
+    def __init__(self,weights,bias):
+        self.bias = bias
+        self.weights = weights
+    #evaluate the current neuron against an array of inputs and returns whether
+    #the neuron is activated
+    def eval(self, inputs):
+        assert inputs.__len__() == self.weights.__len__() #make sure there are the same amount of inputs and weights
+        activation = 0
+        for i in range(0,inputs.__len__()): # for every input
+            activation += self.weights[i]*inputs[i]
+        return activation > self.bias #if the activation is greater than the bias, fire the neuron (true)       
 
 
 #TODO EVOLVE STUFF
 
 #TEST CASES
-n=Neuron() #Neuron n= new Neuron();
-print "Our NAND:" #NAND returns true for everything except 1,1,1 (ACTUAL NAND)
-i=n.NAND(0,0,0)
-t1= n.Output(i)
-print t1
-i=n.NAND(0,0,1)
-t2= n.Output(i)
-print t2
-i=n.NAND(0,1,0)
-t3= n.Output(i)
-print t3
-i=n.NAND(1,0,0)
-t4= n.Output(i)
-print t4
-i=n.NAND(0,1,1)
-t5= n.Output(i)
-print t5
-i=n.NAND(1,1,0)
-t6= n.Output(i)
-print t6
-i=n.NAND(1,0,1)
-t7= n.Output(i)
-print t7
-i=n.NAND(1,1,1)
-t8= n.Output(i)
-print t8
-print "Our OR:"
-i=n.OR(0,0,0)
-t1= n.Output(i)
-print t1
-i=n.OR(0,0,1)
-t2= n.Output(i)
-print t2
-i=n.OR(0,1,0)
-t3= n.Output(i)
-print t3
-i=n.OR(1,0,0)
-t4= n.Output(i)
-print t4
-i=n.OR(0,1,1)
-t5= n.Output(i)
-print t5
-i=n.OR(1,1,0)
-t6= n.Output(i)
-print t6
-i=n.OR(1,0,1)
-t7= n.Output(i)
-print t7
-i=n.OR(1,1,1)
-t8= n.Output(i)
-print t8
+nandNeuron = Neuron([-1,-1,-1],-3) #initialize the nand neuron with appropriate weights
+print ("Our NAND:")
+#NAND returns true for everything except 1,1,1 (ACTUAL NAND)
+print (nandNeuron.eval([0,0,0]))
+print (nandNeuron.eval([0,0,1]))
+print (nandNeuron.eval([0,1,0]))
+print (nandNeuron.eval([0,1,1]))
+print (nandNeuron.eval([1,0,0]))
+print (nandNeuron.eval([1,0,1]))
+print (nandNeuron.eval([1,1,0]))
+print (nandNeuron.eval([1,1,1]))
+
+orNeuron = Neuron([1,1,1],0) #initialize the or neuron with appropriate weights and 
+print ("Our OR:")
+print (orNeuron.eval([0,0,0]))
+print (orNeuron.eval([0,0,1]))
+print (orNeuron.eval([0,1,0]))
+print (orNeuron.eval([0,1,1]))
+print (orNeuron.eval([1,0,0]))
+print (orNeuron.eval([1,0,1]))
+print (orNeuron.eval([1,1,0]))
+print (orNeuron.eval([1,1,1]))

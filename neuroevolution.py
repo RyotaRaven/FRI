229919e47b36cc7e1f47c2001b10032f58006efe
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-02-28 15:29:46 -0600

    network evolution! with 8 weights for xor....

--- neuroevolution.py
+++ neuroevolution.py
@@ -8,7 +8,6 @@ class Neuron:
     #the neuron is activated
     def eval(self, inputs):
         inputs.insert(0,1) #add the bias input (always one) to the input array (at the beginning)
-        assert inputs.__len__() == self.weights.__len__() #make sure there are the same amount of inputs and weights
         activation = 0
         for i in range(0,inputs.__len__()): # for every input
             activation += self.weights[i]*inputs[i]
@@ -68,14 +67,75 @@ class NeuralNetwork:
         out=[one,two]
         return self.outNeuron.eval(out)
 
+class NeuralNetworkEvolution:
+    weights1= []
+    weights2= []
+    weights3=[]
+    number_Of_Tweaks=0
+    randomWeights1= []
+    randomWeights2= []
+    randomWeights3= []
+    tweakedWeights1= []
+    tweakedWeights2= []
+    tweakedWeights3= []
+    goalOutput = []
+    activation = 0
+
+    def __init__(self,expected,numTweaks):
+        self.number_Of_Tweaks = numTweaks
+        self.goalOutput = expected
+
+    def getWeights(self):
+        number_Of_Tweaks=0
+        randomWeights1= []
+        randomWeights2= []
+        randomWeights3= []
+        tweakedWeights1= []
+        tweakedWeights2= []
+        tweakedWeights3= []
+        for i in range(0, 4):
+            self.randomWeights1.append(random.randint(-10,10))
+            self.randomWeights2.append(random.randint(-10,10))
+        for y in range(0,3):
+            self.randomWeights3.append(random.randint(-10,10))
+        self.tweakedWeights1 = self.randomWeights1[:] #use the slice operator to keep from aliasing the lists
+        self.tweakedWeights2 = self.randomWeights2[:]
+        self.tweakedWeights3 = self.randomWeights3[:]
+        n= NeuralNetwork(self.randomWeights1,self.randomWeights2,self.randomWeights3)
+        sameCount = 0 #count the amount of times the quality doesn't progress, and if it is after some threshold, reassign random weights
+        while NetworkQuality(n,self.goalOutput) < 8 :
+            print(self.tweakedWeights1, self.tweakedWeights2,self.tweakedWeights3, NetworkQuality(n,self.goalOutput))
+            if sameCount < 100:
+                self.randomWeights1[random.randint(0,len(self.randomWeights1)-1)] += random.randint(-10,10) #increment or decrement a random weight
+                self.randomWeights2[random.randint(0,len(self.randomWeights2)-1)] += random.randint(-10,10) #increment or decrement a random weight
+                self.randomWeights3[random.randint(0,len(self.randomWeights3)-1)] += random.randint(-10,10) #increment or decrement a random weight
+            else:
+                sameCount = 0
+                for j in range(0, 4):
+                    self.randomWeights1[j] = random.randint(-10,10)
+                    self.randomWeights2[j] = random.randint(-10,10)
+                for x in range(0,3):
+                    self.randomWeights3[x] = random.randint(-10,10)
+                self.tweakedWeights1 = self.randomWeights1[:]
+                self.tweakedWeights2 = self.randomWeights2[:]
+                self.tweakedWeights3 = self.randomWeights3[:]
+            n= NeuralNetwork(self.randomWeights1,self.randomWeights2,self.randomWeights3)
+            if NetworkQuality(NeuralNetwork(self.tweakedWeights1,self.tweakedWeights2,self.tweakedWeights3),self.goalOutput) > NetworkQuality(n,self.goalOutput):
+                self.randomWeights1 = self.tweakedWeights1[:]
+                self.randomWeights2 = self.tweakedWeights2[:]
+                self.randomWeights3 = self.tweakedWeights3[:]
+            else:
+                sameCount += 1
+        return  [self.randomWeights1, self.randomWeights2, self.randomWeights3]
+
 
 #return the amount of times the output of a neuron was the same as the expected output that was passed in an array
 #this functions tests all inputs in this form
 #expected[0]==neuron.eval([0,0,0])
 #expected[1]==neuron.eval([0,0,1])
 #expected[2]==neuron.eval([0,1,0]) etc.
+
 def quality(neuron, expected):
-    assert 2**(neuron.weights.__len__()-1) == expected.__len__() #make sure there are the same amount of unique inputs and expected values
     correct = 0
     for i in range(0,(2**(neuron.weights.__len__()-1))):
         total = i
@@ -87,11 +147,25 @@ def quality(neuron, expected):
             correct += 1     #one more of the expected inputs is correct
     return correct
 
-
-evolveNeuron1 = NeuronEvolution([1,1,1,1,1,1,1,0],100) #try to evolve an nand neuron (works eventually, kinda slow)
-print   ("final weights", evolveNeuron1.getWeights())
-#evolveNeuron2 = NeuronEvolution([0,1,1,0,1,0,0,0],100) #try to evolve an nand neuron (i've never seen it work, i guess that means our evolution needs optimazation)
-#print   ("final weights", evolveNeuron2.getWeights())
+def NetworkQuality(neuralNetwork, expected):
+    correct = 0
+    if(neuralNetwork.evaluate([0,0,0])==expected[0]):
+         correct+=1
+    if(neuralNetwork.evaluate([0,0,1])==expected[1]):
+         correct+=1
+    if(neuralNetwork.evaluate([0,1,0])==expected[2]):
+         correct+=1
+    if(neuralNetwork.evaluate([1,0,0])==expected[3]):
+         correct+=1
+    if(neuralNetwork.evaluate([0,1,1])==expected[4]):
+         correct+=1
+    if(neuralNetwork.evaluate([1,1,0])==expected[5]):
+         correct+=1
+    if(neuralNetwork.evaluate([1,0,1])==expected[6]):
+         correct+=1
+    if(neuralNetwork.evaluate([1,1,1])==expected[7]):
+         correct+=1
+    return correct
 
 #TEST CASES
 nandNeuron = Neuron([6,-2,-2,-2]) #initialize the nand neuron with appropriate weights
@@ -131,4 +205,13 @@ print (network.evaluate([1,0,0]))
 print (network.evaluate([1,0,1]))
 print (network.evaluate([1,1,0]))
 print (network.evaluate([1,1,1]))
-print ("Quality of OR:" , quality(orNeuron,[False,True,True,True,True,True,True,True]))
+
+#evolveNeuron1 = NeuronEvolution([1,1,1,1,1,1,1,0],100) #try to evolve an nand neuron (works eventually, kinda slow)
+#print   ("final weights", evolveNeuron1.getWeights())
+#evolveNeuron2 = NeuronEvolution([0,1,1,0,1,0,0,0],100) #try to evolve xOR neuron (i've never seen it work, i guess that means our evolution needs optimazation)
+#print   ("final weights", evolveNeuron2.getWeights())
+evolveNetwork1 = NeuralNetworkEvolution([1,1,1,1,1,1,1,0],100) #try to evolve an nand neural network (works eventually, kinda slow)
+print   ("final weights", evolveNetwork1.getWeights())
+print("\n \n \n")
+evolveNetwork2 = NeuralNetworkEvolution([0,1,1,0,1,0,0,0],100) #try to evolve an xor neural network (i've never seen it work, i guess that means our evolution needs optimazation)
+print   ("final weights", evolveNetwork2.getWeights())
